be3b4547fee10f49655fe7933c80e8fce576968d
Elliot Masor <44781950+elliot-masor@users.noreply.github.com>
2022-03-09 15:52:14 -0800

    force return of 400 when non nullable fields are passed
    https://stackoverflow.com/questions/52444699/kotlin-spring-bean-validation-nullability
    
    Co-authored-by: Elliot Masor <44781950+elliot-masor@users.noreply.github.com>
    Co-authored-by: Autumn Kennedy <autumn.kennedy@tala.co>

--- fake-dependency-service/src/main/kotlin/co/tala/api/fakedependency/model/MockData.kt
+++ fake-dependency-service/src/main/kotlin/co/tala/api/fakedependency/model/MockData.kt
@@ -3,11 +3,14 @@ package co.tala.api.fakedependency.model
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.util.CollectionUtils
+import javax.validation.constraints.NotNull
 
 
 class MockData(
     val responseBody: Any? = null,
+    @field: NotNull
     val responseSetUpMetadata: ResponseSetUpMetadata = ResponseSetUpMetadata(),
+    @field: NotNull
     val responseHeaders: Map<String, List<String>> = emptyMap()
 ) {
     fun toResponseEntity(): ResponseEntity<Any> = ResponseEntity(
