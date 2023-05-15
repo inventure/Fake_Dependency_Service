0013f1cb87666bdb058722976594af493bef7b64
Elliot Masor <44781950+elliot-masor@users.noreply.github.com>
2023-05-15 14:57:39 -0700

    added support for urlencoded form
    Co-authored-by: kai <arkady.fishkind@tala.co>
    Co-authored-by: Elliot Masor <44781950+elliot-masor@users.noreply.github.com>

--- fake-dependency-service/src/main/kotlin/co/tala/api/fakedependency/business/parser/PayloadParser.kt
+++ fake-dependency-service/src/main/kotlin/co/tala/api/fakedependency/business/parser/PayloadParser.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import org.springframework.stereotype.Component
+import java.io.IOException
 
 @Component
 class PayloadParser(
@@ -18,12 +19,21 @@ class PayloadParser(
     private fun findValue(any: Any, keys: List<String>, depth: Int): String? = when {
         keys.size == depth -> any.toString()
         any is Map<*, *> -> findValueInMap(any as Map<String, Any>, keys, depth)
-        any is String -> findValueInXmlString(any.toString(), keys, depth)
+        any is String -> {
+            findValueInXmlString(any.toString(), keys, depth) ?: if (keys.size == 1) {
+                findValueInUrlEncodedForm(any.toString(), keys.first())
+            } else null
+        }
+
         else -> throw PayloadTypeNotSupportedException(any)
     }
 
     private fun findValueInXmlString(xml: String, keys: List<String>, depth: Int): String? {
-        val any = xmlMapper.readValue(xml, Any::class.java)
+        val any = try {
+            xmlMapper.readValue(xml, Any::class.java)
+        } catch (ignored: IOException) {
+            return null
+        }
         val map = mapper.convertValue(any, object : TypeReference<Map<String, Any>>() {})
         return findValueInMap(map, keys, depth)
     }
@@ -34,4 +44,13 @@ class PayloadParser(
         return findValue(child, keys, depth + 1)
     }
 
+    private fun findValueInUrlEncodedForm(form: String, key: String): String? {
+        return form.split("&").asSequence()
+            .map { it.split("=") }
+            .filter { it.size == 2 }
+            .map { it[0] to it[1] }
+            .filter { it.first.isNotEmpty() && it.second.isNotEmpty() }
+            .find { it.first == key }
+            ?.second
+    }
 }
